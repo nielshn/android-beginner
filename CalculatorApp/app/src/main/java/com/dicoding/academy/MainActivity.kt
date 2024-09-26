9a84a18489046f868e75fc5a3844bd49355ea18a
nielshn <niel.shn08@gmail.com>
2024-09-26 13:30:55 +0700

    exercise activity

---
+++ CalculatorApp/app/src/main/java/com/dicoding/academy/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.dicoding.academy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState!= null) {
+            tvResult.text = savedInstanceState.getString(STATE_RESULT)
+        } else {
+            tvResult.text = ""
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidht = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                edtLength.error = "Length is required"
+            }
+            if (inputWidht.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = "Width is required"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                edtHeight.error = "Height is required"
+            }
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidht.toDouble() * inputHeight.toDouble()
+                tvResult.text = "Volume: $volume"
+            }
+        }
+    }
+}
